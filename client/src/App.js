8858fc10ccb9ab5291258a8ecb8595e9d9ee5993
Ulan Rakymzhanov <ulan@carta.healthcare>
2020-01-27 16:04:29 -0800

    98% done

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { 
-    BrowserRouter as Router, 
-    Route } 
-from 'react-router-dom'; 
 
-import Join from './components/Join/Join'
-import Chat from './components/Chat/Chat'
+import Chat from './components/Chat/Chat';
+import Join from './components/Join/Join';
 
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-const App = () => (
+const App = () => {
+  return (
     <Router>
-        <Route path="/" exact component = {Join}/>
-        <Route path="/chat" exact component = {Chat}/>
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" component={Chat} />
     </Router>
-)
+  );
+}
 
 export default App;
